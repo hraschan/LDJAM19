b0ca85126e80cb010a5f0d9912c09fa81e4be285
hraschan <mt171109@fhstp.ac.at>
2019-04-27 15:42:16 +0200

    Finished Random Object spawn

---
+++ Project/Assets/_Scripts/RandomPlayerSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlayerSpawn : MonoBehaviour
+{
+    public GameObject player;
+    private float positionX;
+    private float positionY;
+    [SerializeField]
+    private float negativemaxX;
+    [SerializeField]
+    private float positivemaxX;
+    [SerializeField]
+    private float negativemaxZ;
+    [SerializeField]
+    private float positivemaxZ;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionX = Random.Range(negativemaxX, positivemaxX);
+        positionY = Random.Range(negativemaxZ, positivemaxZ);
+        player.transform.position = new Vector3(positionX, 0.59f, positionY);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
