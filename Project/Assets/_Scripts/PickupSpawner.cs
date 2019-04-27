3c14f8d2b38c3676eefea7044da68490414919f5
hraschan <mt171109@fhstp.ac.at>
2019-04-27 18:25:52 +0200

    Update PickupSpawner.cs

--- Project/Assets/_Scripts/PickupSpawner.cs
+++ Project/Assets/_Scripts/PickupSpawner.cs
@@ -29,45 +29,14 @@ public class PickupSpawner : MonoBehaviour
     }
     public void spawnItem() {
 
-        bool canSpawnhere;
         spawnPositionX = Random.Range(Global.negativemaxX, Global.positivemaxX);
         spawnPositionZ = Random.Range(Global.negativemaxZ, Global.positivemaxZ);
         Vector3 spawnPos = new Vector3(spawnPositionX, 0.59f, spawnPositionZ);
-        canSpawnhere = preventSpawnOverlap(spawnPos);
+
         object1.transform.position = spawnPos;
 
     }
-    void preventSpawnOverlap(Vector3 spawnPos)
-    {
-        colliders = Physics.OverlapSphere(transform.position, raduis);
-
-        for(int i = 0; i < colliders.Length; i++)
-        {
-            Vector3 centerPoint = colliders[i].bounds.center;
-            float width = colliders[i].bounds.extents.x;
-            float length = colliders[i].bounds.extents.z;
-
-            float leftExtent = centerPoint.x - width;
-            float rightExtent = centerPoint.x + width;
-            float lowerExtent = centerPoint.z - length;
-            float upperExtent = centerPoint.z + length;
-
-            if (spawnPos.x >= leftExtent && spawnPos.x <= rightExtent)
-            {
-                if(spawnPos.z >= lowerExtent && spawnPos.z <= upperExtent)
-                {
-                    return false;
-                }
-            }
-            return true ;
-        }
-    }
     
-
-
-
-
-
     // Update is called once per frame
     void Update()
     {
