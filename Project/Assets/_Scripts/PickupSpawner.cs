b426bc5ae1b2021fca73d54fb0739a8ac3aa2872
IneffableChris <50044434+IneffableChris@users.noreply.github.com>
2019-04-27 16:45:51 +0200

    update

---
+++ Project/Assets/_Scripts/PickupSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+    private float spawnPositionX;
+    private float spawnPositionZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPositionX = Random.Range(Global.negativemaxX, Global.positivemaxX);
+        spawnPositionZ = Random.Range(Global.negativemaxZ, Global.positivemaxZ);
+        object1.transform.position = new Vector3(spawnPositionX, 0.59f, spawnPositionZ);
+        object2.transform.position = new Vector3(spawnPositionX, 0.59f, spawnPositionZ);
+        object3.transform.position = new Vector3(spawnPositionX, 0.59f, spawnPositionZ);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
