72264f8edf7a8ea20d2eccef467f4a2f110c0bf6
hraschan <mt171109@fhstp.ac.at>
2019-04-28 11:55:40 +0200

    update

---
+++ Project/Assets/_Scripts/detectedEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectedEnemy : MonoBehaviour
+{
+    private GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+    
+        if(other.gameObject.tag == "player")
+        {
+            //Globaler Bool auf true - Danach wird followPlayer ausgeführt.
+            Global.enemy = true;
+        }
+    }
+}
