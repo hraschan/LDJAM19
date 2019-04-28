b7c116a5835503adb1c8a246073379ced18802a9
IneffableChris <50044434+IneffableChris@users.noreply.github.com>
2019-04-28 18:13:42 +0200

    j

---
+++ Project/Assets/_Scripts/PlayerHealthScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthScript : MonoBehaviour
+{
+    public float max_Health = 100f;
+    public float cur_Health = 0f;
+
+    public GameObject healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cur_Health = max_Health;
+        InvokeRepeating("decreasehealth", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void decreasehealth()
+    {
+        cur_Health -= 5f;
+        float calc_Health = cur_Health / max_Health;
+        SetHealthBar(calc_Health);
+    }
+
+    public void SetHealthBar(float myHealth)
+    {
+        healthBar.transform.localScale = new Vector3(Mathf.Clamp(myHealth, 0f, 1f),
+                                                     healthBar.transform.localScale.y,
+                                                     healthBar.transform.localScale.z);
+    }
+}
