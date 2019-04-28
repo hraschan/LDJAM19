cea32210a3acd04484dfa210f2b127ed2ded5c46
Daniel Bründl <dbruendl@hotmail.com>
2019-04-28 17:53:11 +0200

    Startmenu

---
+++ Project/Assets/_Scripts/GameManager_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager_Menu : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
