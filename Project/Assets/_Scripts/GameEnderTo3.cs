c04215f6f286709e01a1af66f833d99ffdefe85b
IneffableChris <50044434+IneffableChris@users.noreply.github.com>
2019-04-29 18:54:48 +0200

    ufuf

---
+++ Project/Assets/_Scripts/GameEnderTo3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameEnderTo3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Yahuuuuuuu");
+            SceneManager.LoadScene("Level 3");
+        }
+    }
+}
