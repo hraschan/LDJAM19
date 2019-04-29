ba83da368119962f728ba9839e4f8062e04e9eed
IneffableChris <50044434+IneffableChris@users.noreply.github.com>
2019-04-29 11:36:54 +0200

    zusammenfassen

---
+++ Project/Assets/_Scripts/testscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    GameObject objToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objToSpawn = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
