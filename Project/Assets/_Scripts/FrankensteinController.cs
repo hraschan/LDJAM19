c2cffec1ec90fb02c252cd29016eb1e900ccad5a
Mimipoint <50044424+Mimipoint@users.noreply.github.com>
2019-04-29 22:04:45 +0200

    movement changed
    changed movement from translate to addforce (adjustments need to be made)

--- Project/Assets/_Scripts/FrankensteinController.cs
+++ Project/Assets/_Scripts/FrankensteinController.cs
@@ -4,20 +4,41 @@ using UnityEngine;
 
 public class FrankensteinController : MonoBehaviour
 {
-    public float moveSpeed = 3f;
+    [SerializeField]
+    int speedLimit;
+
+    public float moveSpeed;
+    private Rigidbody r;
+
     // Use this for initialization
     void Start()
     {
-
+        r = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Moves Forward and back along z axis                           //Up/Down 
-        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        //transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        float x = Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed;
+        if (Input.GetAxis("Horizontal") == 0) r.velocity = new Vector3(0, 0, r.velocity.z);
+        float z = Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed;
+        if (Input.GetAxis("Vertical") == 0) r.velocity = new Vector3(r.velocity.x, 0, 0);
+
+        Debug.Log(x + " " + z);
+
+        Vector3 speed = r.velocity;
+        float newX = Mathf.Clamp(speed.x, -speedLimit, speedLimit);
+        float newZ = Mathf.Clamp(speed.z, -speedLimit, speedLimit);
+
+        r.velocity = new Vector3(newX, 0, newZ);
+
+        this.GetComponent<Rigidbody>().AddForce(x, 0, z);
+
+
         //Moves Left and right along x Axis                               //Left/Right
-        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        //transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
 
 
        
